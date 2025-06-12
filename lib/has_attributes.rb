b141a2d20e7b0d49baa9f19706a69fd241718708
Rahoul Baruah <rahoulb@echodek.co>
2025-06-12 21:57:53 +0100

    Store models as attributes
    Unlike storing models as an attribute, using `has_model` lazy loads the
    model from the database, reducing the load

--- lib/has_attributes.rb
+++ lib/has_attributes.rb
@@ -2,6 +2,7 @@
 
 require_relative "has_attributes/version"
 require "active_support/concern"
+require "active_support/core_ext/string/inflections"
 require "global_id_serialiser"
 
 module HasAttributes
@@ -9,7 +10,9 @@ module HasAttributes
   class Error < StandardError; end
 
   class_methods do
-    def has_attribute name, cast_type = :string, field_name: "data", **options
+    def has_attribute name, cast_type = :string, field_name: :data, **options
+      field_name = field_name.to_sym
+      name = name.to_sym
       typecaster = cast_type.nil? ? nil : ActiveRecord::Type.lookup(cast_type)
       typecast_value = ->(value) { typecaster.nil? ? value : typecaster.cast(value) }
       define_attribute_method name
@@ -25,6 +28,29 @@ module HasAttributes
       define_method(:"#{name}=") do |value|
         attribute_will_change! name
         send(field_name.to_sym)[name.to_s] = typecast_value.call(value)
+      end
+    end
+
+    def has_model name, class_name = nil, field_name: :data, **options
+      name = name.to_sym
+      id_attribute = :"#{name}_id"
+      has_attribute id_attribute, field_name:, **options
+      validate :"#{name}_class_name", if: -> { send(name).present? } if class_name.present?
+
+      define_method(name.to_sym) do
+        model_id = send id_attribute
+        model_id.nil? ? nil : GlobalID::Locator.locate(model_id.sub("modelid-", ""))
+      rescue ActiveRecord::RecordNotFound
+        nil
+      end
+
+      define_method(:"#{name}=") do |model|
+        model_id = model.nil? ? nil : "modelid-#{model.to_global_id}"
+        send :"#{id_attribute}=", model_id
+      end
+
+      define_method :"#{name}_class_name" do
+        errors.add name, :invalid unless send(name).is_a?(class_name.constantize)
       end
     end
   end
--- spec/has_attributes_spec.rb
+++ spec/has_attributes_spec.rb
@@ -134,4 +134,66 @@ RSpec.describe HasAttributes do
       expect(item.friendly?).to be true
     end
   end
+
+  context "defining attributes that hold models or other GlobalIDs" do
+    before do
+      GlobalID.app = "global_id_serialiser"
+      FileUtils.rm_rf "tmp/test.sqlite3"
+      ActiveRecord::Base.establish_connection adapter: :sqlite3, database: "tmp/test.sqlite3"
+      ActiveRecord::Base.connection.create_table :items do |t|
+        t.string :name
+        t.text :data
+      end
+      ActiveRecord::Base.connection.create_table :things do |t|
+        t.string :name
+        t.string :type
+        t.text :data
+      end
+      # standard:disable Lint/ConstantDefinitionInBlock
+      class Item < ActiveRecord::Base
+        include HasAttributes
+        include GlobalID::Identification
+        serialize :data, type: Hash, coder: GlobalIdSerialiser
+
+        has_attribute :greeting, :string, default: "Hello"
+        has_model :thing, "Thing"
+      end
+
+      class Thing < ActiveRecord::Base
+        include GlobalID::Identification
+      end
+
+      class SubThing < Thing
+      end
+      # standard:enable Lint/ConstantDefinitionInBlock
+    end
+
+    it "allows models to be set" do
+      thing = Thing.create name: "A thing"
+      item = Item.new name: "Dave", thing: thing
+
+      expect(item.thing).to eq thing
+    end
+
+    it "allows sub-classes to be stored" do
+      sub_thing = SubThing.create name: "A sub thing"
+      item = Item.create! name: "Ellie", thing: sub_thing
+
+      item.thing = sub_thing
+    end
+
+    it "does not allow different classes to be stored" do
+      not_a_thing = Item.create! name: "Not a thing"
+      item = Item.new name: "Fred", thing: not_a_thing
+
+      expect(item).to_not be_valid
+      expect(item.errors).to include :thing
+    end
+
+    it "allows nil to be stored" do
+      item = Item.new name: "Georgia", thing: nil
+
+      expect(item.thing).to be_nil
+    end
+  end
 end
